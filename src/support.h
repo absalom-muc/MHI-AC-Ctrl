c004534b722f93e882d3cd3861b517be4cc18957
metarutaiga <52752725+metarutaiga@users.noreply.github.com>
2020-06-15 12:03:52 +0800

    Remove support for ESP-01

--- src/support.h
+++ src/support.h
@@ -23,8 +23,6 @@
 #define TEMP_MEASURE_PERIOD 30                      // period in seconds for temperature measurement with the external DS18x20 temperature sensor
                                                     // enter 0 if you don't use the DS18x20 
 
-#define BOARD_ESP01 0                               // the board is ESP-01 that 4 pins only
-
 #include <ESP8266WiFi.h>        // https://github.com/esp8266/Arduino/tree/master/libraries/ESP8266WiFi
 #include <PubSubClient.h>       // https://github.com/knolleary/pubsubclient
 #include <ArduinoOTA.h>         // https://github.com/esp8266/Arduino/tree/master/libraries/ArduinoOTA
@@ -34,15 +32,10 @@
 #include <DallasTemperature.h>  // https://github.com/milesburton/Arduino-Temperature-Control-Library
 #endif
 
-#if BOARD_ESP01 > 0
-#define SCK_PIN  3
-#define MOSI_PIN 0
-#define MISO_PIN 2
-#else
+// pin defintions
 #define SCK_PIN  14
 #define MOSI_PIN 13
 #define MISO_PIN 12
-#endif
 
 extern PubSubClient MQTTclient;
 
